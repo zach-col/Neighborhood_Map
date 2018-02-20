c7d8e57643df6cf3f6d74394436bd026ddc97c33
zach <theperfectpicture100@gmail.com>
2018-02-19 23:31:26 -0600

    fix: typo with FourSquare API insert comment

--- js/googleMaps.js
+++ js/googleMaps.js
@@ -77,7 +77,7 @@ function populateInfoWindow(marker, infowindow) {
       marker.lng +
       "&query=" +
       marker.title +
-      "&v=20150214&m=foursquare&client_secret=INSERT_FOURSQUARE_API_CLIENT_SECRET_HERE&client_id=INSERT_FOURSQUARE_API_CLIEND_ID_HERE";
+      "&v=20150214&m=foursquare&client_secret=INSERT_FOURSQUARE_API_CLIENT_SECRET_HERE&client_id=INSERT_FOURSQUARE_API_CLIENT_ID_HERE";
 
       // json
       $.getJSON(fourSquareRequest).done(function(marker){
